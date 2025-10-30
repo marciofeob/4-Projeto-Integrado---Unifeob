1e09f6041323d4930c1fef3d88dae7a4391439f1
Gustavo Donadello <gustavodonadello@hotmail.com>
2025-10-30 20:11:31 -0300

    FrontEnd

--- renderer/public/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-const user = JSON.parse(localStorage.getItem('user'));
-if (!user) window.location.href = 'login.html';
-
-document.getElementById('userName').textContent = user.name;
-
-function logout() {
-  localStorage.removeItem('user');
-  window.location.href = 'login.html';
-}
